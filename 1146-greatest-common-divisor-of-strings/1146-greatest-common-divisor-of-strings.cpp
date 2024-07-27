5fcb5a01f5becd386fc488a0e8491b5d226c11a5
sourishgrover <161309652+sourishgrover@users.noreply.github.com>
2024-07-27 11:19:18 +0530

    Time: 5 ms (45.63%), Space: 8.5 MB (85.64%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        int n = str1.length();
+        int m = str2.length();
+        if(str1+str2 != str2+str1){
+            return "";
+        }else{
+            return str1.substr(0,gcd(n,m));
+        }
+    }
+};
