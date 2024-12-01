1c7f19be2cd1cc3797852059386debe14dc85cb1
Sourish_Grover <161309652+sourishgrover@users.noreply.github.com>
2024-12-01 19:35:42 +0530

    Time: 0 ms (100%), Space: 11.5 MB (5.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,29 +2,49 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
+    //    optimal
+        // int zeros = 0 ,ones = 0,twos = 0;
 
-        int zeros = 0 ,ones = 0,twos = 0;
+        // for(int i = 0 ; i < n ; i++){
+        //     if(nums[i]==0){
+        //        zeros++;
+        //     }
+        //     else if(nums[i]==1){
+        //        ones++;
+        //     }
+        //     else{
+        //         twos++;
+        //     }
+        // }
+        //  int index = 0;
+        //  for(int i=0; i < zeros ; i++){
+        //     nums[index++]=0;
+        //  }
+        //  for(int i=0; i < ones ; i++){
+        //     nums[index++]=1;
+        //  }
+        //  for(int i=0; i < twos ; i++){
+        //     nums[index++]=2;
+        //  }
 
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i]==0){
-               zeros++;
+
+        // DNF
+        int mid = 0 , low = 0 , high = n-1;
+
+        while(mid<=high){
+
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                mid++;
+                low++;
             }
-            else if(nums[i]==1){
-               ones++;
+            else if(nums[mid]==1){
+                 mid++;
             }
             else{
-                twos++;
+                swap(nums[high],nums[mid]);
+                high--;
             }
         }
-         int index = 0;
-         for(int i=0; i < zeros ; i++){
-            nums[index++]=0;
-         }
-         for(int i=0; i < ones ; i++){
-            nums[index++]=1;
-         }
-         for(int i=0; i < twos ; i++){
-            nums[index++]=2;
-         }
     }
 };
