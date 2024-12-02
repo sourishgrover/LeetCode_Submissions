5e5ca03969ec838e36de589bad3e2aa4b2c2e39c
Sourish_Grover <161309652+sourishgrover@users.noreply.github.com>
2024-12-02 12:36:42 +0530

    Time: 0 ms (100%), Space: 12.3 MB (5.14%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,21 +4,11 @@ public:
         int index = m+n-1 , i = m-1 , j = n-1;
   
          while(i>=0 && j >=0){
-            if(nums1[i] >= nums2[j]){
-                 nums1[index] = nums1[i];
-                index--;
-                i--;
-                
-            }else{
-                 nums1[index]= nums2[j];
-                index--;
-                j--;
-            }
+          nums1[index--] = (nums1[i] >= nums2[j]) ? nums1[i--] : nums2[j--];
          }
           while(j>=0){
-                 nums1[index]= nums2[j];
-                index--;
-                j--;
+                 nums1[index--]= nums2[j--];
+               
             }
         
     }
