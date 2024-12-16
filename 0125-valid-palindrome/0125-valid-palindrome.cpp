b7efcfbe46c9936edb2bdf2e81021dfac37bd0f6
Sourish_Grover <161309652+sourishgrover@users.noreply.github.com>
2024-12-16 23:55:25 +0530

    Time: 4 ms (15.94%), Space: 10 MB (12.14%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+   bool isAlphaNum(char ch){
+    if((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
+        return true;
+    }
+    return false;
+   }
+
+    
+    bool isPalindrome(string s) {
+
+        int n = s.length()-1;
+        int st = 0 , end = n;
+        
+         while(st<end){
+            if(!isAlphaNum(s[st])){
+                st++; continue;
+            } 
+            if(!isAlphaNum(s[end])){
+                 end--; continue;
+            }
+
+            if(tolower(s[st]) != tolower(s[end])){
+                return false;
+            }
+            st++; end--;
+         }
+        return true;
+    }
+};
